cd35f55431620107e000ca99fb3607096b833e72
Vladimir Dobrikov <vdobrikov@okta.com>
2016-05-28 23:19:50 +0300

    Add readme Code polishing

--- MQTTNode.cpp
+++ MQTTNode.cpp
@@ -1,30 +1,34 @@
 #include "MQTTNode.h"
 
+MQTTNode::MQTTNode(const char* name, const char* host){
+
+	init(name, host, DEFAULT_MQTT_PORT);
+}
+
 MQTTNode::MQTTNode(const char* name, const char* host, uint16_t port){
-	
+
 	init(name, host, port);
 }
 
-MQTTNode::MQTTNode(const char* name, const char* host, uint16_t port, const char* username, const char* password){
-	
+MQTTNode::MQTTNode(const char* name, const char* host, uint16_t port, const char* mqttUsername, const char* mqttPassword){
 	init(name, host, port);
-	this->mqtt_username = username;
-	this->mqtt_password = password;
+	this->mqtt_username = mqttUsername;
+	this->mqtt_password = mqttPassword;
 }
 
 void MQTTNode::init(const char* name, const char* host, uint16_t port) {
-
 	this->name = name;
-	this->mqtt_host = host;
-	this->mqtt_port = port;
+	client.setServer(host, port);
 }
 
-PubSubClient MQTTNode::getClient() {
-
-	return client;
+PubSubClient* MQTTNode::getClient() {
+	return &client;
 }
 
 void MQTTNode::reconnect() {
+	if (name == NULL) {
+		name = DEFAULT_NODE_NAME;
+	}
 
 	// Loop until we're reconnected
 	while (!client.connected()) {
@@ -46,19 +50,45 @@ void MQTTNode::reconnect() {
 	}
 }
 
+bool MQTTNode::isMqttConnected() {
+	return client.connected();
+}
+
+
+
+ESP8266MQTTNode::ESP8266MQTTNode(const char* name, const char* host) :
+	MQTTNode(name, host) {
+
+	this->client.setClient(this->wifiClient);
+}
+
 ESP8266MQTTNode::ESP8266MQTTNode(const char* name, const char* host, uint16_t port) :
 	MQTTNode(name, host, port) {
 
 	this->client.setClient(this->wifiClient);
 }
 
-ESP8266MQTTNode::ESP8266MQTTNode(const char* name, const char* host, uint16_t port, const char* username, const char* password) :
-	MQTTNode(name, host, port, username, password) {
+ESP8266MQTTNode::ESP8266MQTTNode(const char* name, const char* host, uint16_t port, const char* mqttUsername, const char* mqttPassword) :
+	MQTTNode(name, host, port, mqttUsername, mqttPassword) {
 
 	this->client.setClient(this->wifiClient);
 }
 
-void ESP8266MQTTNode::setupWifi(const char* ssid, const char* password) {
+ESP8266MQTTNode::ESP8266MQTTNode(const char* host, const char* mqttUsername, const char* mqttPassword, const char* wifiSsid, const char* wifiPassword, MQTTNodeOnConnectedCallback connectCallback, MQTT_CALLBACK_SIGNATURE) :
+	MQTTNode(NULL, host, DEFAULT_MQTT_PORT, mqttUsername, mqttPassword) {
+
+	this->wifi_ssid = wifiSsid;
+	this->wifi_password = wifiPassword;
+	this->connectedCallback = connectCallback;
+	this->client.setCallback(callback);
+	this->client.setClient(wifiClient);
+}
+
+const char* ESP8266MQTTNode::generateNodeName() {
+  return String(ESP.getChipId()).c_str();
+}
+
+void ESP8266MQTTNode::setupWifi(const char* ssid, const char* wifiPassword) {
 
 	delay(10);
 	// We start by connecting to a WiFi network
@@ -66,44 +96,51 @@ void ESP8266MQTTNode::setupWifi(const char* ssid, const char* password) {
 	MQTTNODE_PRINT("Connecting to ");
 	MQTTNODE_PRINTLN(ssid);
 
-	WiFi.begin(ssid, password);
+	WiFi.begin(ssid, wifiPassword);
 
 	uint32_t connectionTimeoutMs = 10000;
-	uint32_t connectionAttemptStart = millis();
+	uint32_t lastConnectionRetry = millis();
 
 	while (WiFi.status() != WL_CONNECTED) {
 		delay(500);
 		MQTTNODE_PRINT(".");
 
-		if (millis() - connectionAttemptStart > connectionTimeoutMs) {
+		if (millis() - lastConnectionRetry > connectionTimeoutMs) {
 			break;
 		}
 	}
 
 	MQTTNODE_PRINTLN("");
-	MQTTNODE_PRINTLN("WiFi connected");
+	MQTTNODE_PRINT("WiFi connected: ");
+	MQTTNODE_PRINTLN(WiFi.status() == WL_CONNECTED);
 	MQTTNODE_PRINTLN("IP address: ");
 	MQTTNODE_PRINTLN(WiFi.localIP());
 }
 
-void ESP8266MQTTNode::connect(const char* ssid, const char* password, MQTTNodeOnConnectedCallback connectCallback, MQTT_CALLBACK_SIGNATURE) {
+bool ESP8266MQTTNode::isWifiConnected() {
+	return WiFi.status() == WL_CONNECTED;
+}
 
-	MQTTNODE_PRINTLN("Connect");
-	setupWifi(ssid, password);
-	MQTTNODE_PRINTLN("Set server");
-	client.setServer(mqtt_host, mqtt_port);
-	MQTTNODE_PRINTLN("Set callback");
-	client.setCallback(callback);
-	this->connectedCallback = connectCallback;
+void ESP8266MQTTNode::begin() {
+	if (name == NULL) {
+		name = generateNodeName();
+	}
+	MQTTNODE_PRINTLN("Connecting");
+	setupWifi(wifi_ssid, wifi_password);
 
 	// Ensure the client is connected
-	loop();
+	reconnect();
 }
 
 void ESP8266MQTTNode::loop() {
 
+	// Ensure we're still connected to WiFi
+	if (!isWifiConnected()) {
+		setupWifi(wifi_ssid, wifi_password);
+	}
+
 	// Ensure we're still connected to the MQTT server
-	if (!client.connected()) {
+	if (!isMqttConnected()) {
 		reconnect();
 	}
 
--- MQTTNode.h
+++ MQTTNode.h
@@ -20,6 +20,9 @@
 #define MQTTNODE_PRINTLN(str)
 #endif
 
+#define DEFAULT_MQTT_PORT 1883
+#define DEFAULT_NODE_NAME "MQTTNode"
+
 // Forward definition
 class MQTTNode;
 
@@ -28,9 +31,7 @@ typedef void(*MQTTNodeOnConnectedCallback)(MQTTNode&);
 class MQTTNode {
 
 	protected:
-		const char* name = "MQTTNode";
-		const char* mqtt_host = NULL;
-		int mqtt_port = 1883;
+		const char* name = NULL;
 		const char* mqtt_username = NULL;
 		const char* mqtt_password = NULL;
 
@@ -39,13 +40,15 @@ class MQTTNode {
 
 		void init(const char*, const char*, uint16_t);
 		void reconnect();
+        bool isMqttConnected();
 
 	public:
+        MQTTNode(const char*, const char*);
 		MQTTNode(const char*, const char*, uint16_t);
 		MQTTNode(const char*, const char*, uint16_t, const char*, const char*);
 
 		virtual void loop() = 0;
-		PubSubClient getClient();
+		PubSubClient* getClient();
 };
 
 class ESP8266MQTTNode : public MQTTNode{
@@ -57,13 +60,18 @@ class ESP8266MQTTNode : public MQTTNode{
 		const char* wifi_password = NULL;
 
 		void setupWifi(const char*, const char*);
+        bool isWifiConnected();
+        const char* generateNodeName();
 
 	public:
+        ESP8266MQTTNode(const char*, const char*);
 		ESP8266MQTTNode(const char*, const char*, uint16_t);
 		ESP8266MQTTNode(const char*, const char*, uint16_t, const char*, const char*);
-		void connect(const char*, const char*, MQTTNodeOnConnectedCallback, MQTT_CALLBACK_SIGNATURE);
-		virtual void loop();	
-	
+        ESP8266MQTTNode(const char*, const char*, const char*, const char*, const char*, MQTTNodeOnConnectedCallback, MQTT_CALLBACK_SIGNATURE);
+
+		void begin();
+		virtual void loop();
+
 };
 
 
